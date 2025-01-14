b8f1fcafa36181ce1341345dd2b4e48f7d8a0479
sivasdp <sdpsiva191@gmail.com>
2025-01-14 11:11:24 +0530

    change index

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,18 +6,9 @@ import dynamic from "next/dynamic";
 export default function Home() {
   return (
     <>
-      <main className="font-mono flex flex-col justify-center items-center  w-screen">
+      <main className="font-mono flex flex-col justify-center items-center w-screen">
         <h1 className="m-5 text-xl font-bold">Real-Time Object Detection</h1>
         <Yolo />
-        <p className="m-5">
-          Created by{" "}
-          <a
-            className="underline underline-offset-1 hover:translate-y-1"
-            href="https://juanjaho.github.io/"
-          >
-            @juanjaho
-          </a>
-        </p>
       </main>
     </>
   );
